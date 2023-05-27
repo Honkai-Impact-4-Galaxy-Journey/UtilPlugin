43c5681a14b826f5d24ab73763f1f659081978a9
Tiphereth <atmlz32@163.com>
2023-05-27 21:36:39 +0800

    add command killme

---
+++ util/Command.cs
@@ -0,0 +1,29 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSystem
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class Command : ICommand
+    {
+        public string[] Aliases => Array.Empty<string>();
+
+        public string Description => "防卡死指令";
+
+        string ICommand.Command => "killme";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get((sender as CommandSender).SenderId);
+            player.Kill("防卡死指令");
+            response = "Success!";
+            return true;
+        }
+
+    }
+}
