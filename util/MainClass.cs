4ac49cb4ea6e2319867c24421be50335609ab81d
Tiphereth <atmlz32@163.com>
2023-05-27 20:02:59 +0800

    white something

---
+++ util/MainClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled;
+using Exiled.API;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events;
+
+namespace util
+{
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; }
+        public bool Debug { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+    public class UtilPlugin : Plugin<PluginConfig>
+    {
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            
+        }
+    }
+}
