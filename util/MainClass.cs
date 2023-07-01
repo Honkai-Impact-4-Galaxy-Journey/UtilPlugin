9b6eca4591c4330ab4a81cadd8292c3f20ed53b2
Tiphereth <atmlz32@163.com>
2023-07-01 11:42:12 +0800

    add systemwarhead

--- util/MainClass.cs
+++ util/MainClass.cs
@@ -10,7 +10,7 @@ using Exiled.API.Features;
 using Exiled.API.Interfaces;
 using Exiled.Events;
 
-namespace util
+namespace UtilPlugin
 {
     public class PluginConfig : IConfig
     {
@@ -19,6 +19,12 @@ namespace util
         public bool Debug { get; set; }
         [Description("每次自动清理的时间（以秒为单位）")]
         public float Cleanuptime { get; set; } = 500;
+        [Description("是否启用系统核弹")]
+        public bool SystemWarheadEnabled { get; set; } = true;
+        [Description("系统核弹触发时间")]
+        public float SysteamWarheadTime { get; set; } = 1200;
+        [Description("系统核弹触发时公告")]
+        public string SystemWarheadBroadcast { get; set; } = "注意，系统核弹已经启动";
     }
     public class UtilPlugin : Plugin<PluginConfig>
     {
@@ -29,7 +35,7 @@ namespace util
         }
         public override string Author => "Silver Wolf";
         public override string Name => "UtilPlugin";
-        public override Version Version => new Version(1,0,2);
+        public override Version Version => new Version(1,1,0);
         public static UtilPlugin Instance { get; private set; }
         public UtilPlugin()
         {
---
+++ util/SystemWarhead.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilPlugin
+{
+    public class SystemWarhead
+    {
+        public static CoroutineHandle _systemwarheadwaiter;
+        public static void OnRoundStarted()
+        {
+            _systemwarheadwaiter = Timing.RunCoroutine(SystemWarheadwaiter(UtilPlugin.Instance.Config.SysteamWarheadTime));
+        }
+        public static void OnRoundFinished()
+        {
+            Timing.KillCoroutines(_systemwarheadwaiter);
+        }
+        public static IEnumerator<float> SystemWarheadwaiter(float time)
+        {
+            yield return Timing.WaitForSeconds(time);
+            Detonate();
+        }
+        public static void Detonate()
+        {
+            Warhead.LeverStatus = true;
+            Warhead.IsKeycardActivated = true;
+            if (!Warhead.IsInProgress)
+            {
+                Warhead.Start();
+            }
+            Warhead.IsLocked = true;
+            PluginAPI.Core.Server.SendBroadcast(UtilPlugin.Instance.Config.SystemWarheadBroadcast, 10, Broadcast.BroadcastFlags.Normal, true);
+        }
+    }
+}
