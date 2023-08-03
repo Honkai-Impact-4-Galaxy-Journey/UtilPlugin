fffc571c4780c73335195dd9a066da27766ce121
Tiphereth <atmlz32@163.com>
2023-08-03 23:37:03 +0800

    inst

---
+++ util/Voting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace UtilPlugin
+{
+    public class Voting
+    {
+        public static Dictionary<string,VotingEvent> keyValuePairs = new Dictionary<string,VotingEvent>();
+        public static void Register(VotingEvent votingEvent)
+        {
+
+        }
+    }
+    public class VotingEvent
+    {
+        public static Action Action { get; set; }
+        public static string Name { get; set; }
+        public static string Description { get; set; }
+    }
+}
