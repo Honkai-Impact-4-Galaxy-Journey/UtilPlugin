b7026b6755f7e8f6e0f0ac3a4d233a8a21774c29
Tiphereth <atmlz32@163.com>
2023-08-23 23:25:28 +0800

    inst

---
+++ util/RainbowTag.cs
@@ -0,0 +1,28 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilPlugin
+{
+    public class RainbowTag
+    {
+        public static Dictionary<Player, CoroutineHandle> keyValuePairs = new Dictionary<Player, CoroutineHandle>();
+        public static string[] Colors = new string[] { "red", "pink", "pumpkin", "yellow", "light_green", "aqua", "cyan", "silver" };
+        public static IEnumerator<float> OnEnable(Player player)
+        {
+            while (true)
+            {
+                foreach (string s in Colors)
+                {
+                    player.RankColor = s;
+                    yield return Timing.WaitForSeconds(0.25f);
+                }
+                yield return Timing.WaitForSeconds(0.25f);
+            }
+        }
+    }
+}
