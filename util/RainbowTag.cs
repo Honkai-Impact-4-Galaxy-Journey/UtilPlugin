b31c738744c47abc71dbb13bf0014a67d0d61fb0
Tiphereth <atmlz32@163.com>
2023-08-24 21:20:48 +0800

    fix

--- util/RainbowTag.cs
+++ util/RainbowTag.cs
@@ -12,16 +12,16 @@ namespace UtilPlugin
     {
         public static Dictionary<Player, CoroutineHandle> keyValuePairs = new Dictionary<Player, CoroutineHandle>();
         public static string[] Colors = new string[] { "red", "pink", "pumpkin", "yellow", "light_green", "aqua", "cyan", "silver" };
-        public static IEnumerator<float> OnEnable(Player player)
+        public static IEnumerator<float> Register(Player player)
         {
             while (true)
             {
                 foreach (string s in Colors)
                 {
                     player.RankColor = s;
-                    yield return Timing.WaitForSeconds(0.25f);
+                    yield return Timing.WaitForSeconds(0.3f);
                 }
-                yield return Timing.WaitForSeconds(0.25f);
+                yield return Timing.WaitForSeconds(0.3f);
             }
         }
     }
