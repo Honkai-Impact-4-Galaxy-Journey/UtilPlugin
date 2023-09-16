ac0939a87504747272be24da983e4b051b1441d5
Tiphereth <atmlz32@163.com>
2023-09-16 13:18:02 +0800

    inst

---
+++ util/OmegaWarhead.cs
@@ -0,0 +1,16 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilPlugin
+{
+    public class OmegaWarhead
+    {
+        public static List<Player> Helikopter = new List<Player>();
+        public static List<Player> Shelted = new List<Player>();
+
+    }
+}
