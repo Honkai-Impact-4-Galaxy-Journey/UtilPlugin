23fc3de3e2af462a8ac4f1cd1c1ebc121c2bdd26
Tiphereth <atmlz32@163.com>
2023-10-15 22:46:36 +0800

    fix bug

--- util/ReserveSlot.cs
+++ util/ReserveSlot.cs
@@ -13,21 +13,17 @@ namespace UtilPlugin
 {
     public class ReserveSlot
     {
-        public static int Remain = UtilPlugin.Instance.Config.Slots;
+        public static int Remain => UtilPlugin.Instance.Config.Slots - Player.List.Where(p => CheckPermission(p.UserId)).Count();
         public static CoroutineHandle _autorefresh;
         public static void Register(bool reg)
         {
             if (reg)
             {
                 Exiled.Events.Handlers.Player.PreAuthenticating += OnPreAuthenticating;
-                Exiled.Events.Handlers.Player.Left += Disconnecting;
-                Exiled.Events.Handlers.Server.RestartingRound += RoundRestart;
             }
             else
             {
                 Exiled.Events.Handlers.Player.PreAuthenticating -= OnPreAuthenticating;
-                Exiled.Events.Handlers.Player.Left -= Disconnecting;
-                Exiled.Events.Handlers.Server.RestartingRound -= RoundRestart;
             }
         }
 
@@ -39,44 +35,8 @@ namespace UtilPlugin
                 {
                     ev.Reject(UtilPlugin.Instance.Config.ReserveSlotKickReason, true);
                 }
-                else
-                {
-                    Remain--;
-                }
             }
         }
-
-        public static void RoundRestart()
-        {
-            Timing.KillCoroutines(_autorefresh);
-            Remain = UtilPlugin.Instance.Config.Slots;
-            _autorefresh = Timing.RunCoroutine(Autorefresher(60));
-        }
-
-        public static IEnumerator<float> Autorefresher(int time)
-        {
-            while (true)
-            {
-                yield return Timing.WaitForSeconds(time);
-                Remain = UtilPlugin.Instance.Config.Slots;
-                foreach (Player player in Player.List)
-                {
-                    if (CheckPermission(player.UserId))
-                    {
-                        Remain--;
-                    }
-                }
-            }
-        }
-
-        public static void Disconnecting(LeftEventArgs ev)
-        {
-            if (CheckPermission(ev.Player.UserId))
-            {
-                Remain++;
-            }
-        }
-
         public static bool CheckPermission(string userid)
         {
             if (string.Equals("yes", Database.GetBadge(userid).reverseslot, StringComparison.CurrentCultureIgnoreCase))
@@ -92,30 +52,6 @@ namespace UtilPlugin
 }
 namespace CommandSystem
 {
-    [CommandHandler(typeof(GameConsoleCommandHandler))]
-    [CommandHandler(typeof(RemoteAdminCommandHandler))]
-    public class Refresh : ICommand
-    {
-        public string Command => "refresh";
-
-        public string[] Aliases => Array.Empty<string>();
-
-        public string Description => "刷新预留位剩余";
-
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-        {
-            UtilPlugin.ReserveSlot.RoundRestart();
-            foreach (Player player in Player.List)
-            {
-                if (UtilPlugin.ReserveSlot.CheckPermission(player.UserId))
-                {
-                    UtilPlugin.ReserveSlot.Remain--;
-                }
-            }
-            response = $"Remain:{UtilPlugin.ReserveSlot.Remain}";
-            return true;
-        }
-    }
     [CommandHandler(typeof(ClientCommandHandler))]
     [CommandHandler(typeof(GameConsoleCommandHandler))]
     [CommandHandler(typeof(RemoteAdminCommandHandler))]
@@ -138,27 +74,4 @@ namespace CommandSystem
             return true;
         }
     }
-    [CommandHandler(typeof(GameConsoleCommandHandler))]
-    [CommandHandler(typeof(RemoteAdminCommandHandler))]
-    public class CheckPermission : ICommand
-    {
-        public string Command => "setremain";
-
-        public string[] Aliases => Array.Empty<string>();
-
-        public string Description => "";
-
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-        {
-            if (arguments.Count != 1)
-            {
-                response = "need 1 argument!Usage:setremain <newremain>";
-                return false;
-            }
-            UtilPlugin.ReserveSlot.Remain = int.Parse(arguments.At(0));
-            response = "Done!";
-            return true;
-        }
-    }
-    
 }
